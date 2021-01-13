d888dd6a5222ddbe78418f68778b21eeb9e5fa39
Guillermo Garcia <ggq0107@hotmail.com>
2021-01-13 17:42:08 -0500

    created javascript logic for get a new question

--- Assets/quiz.js
+++ Assets/quiz.js
@@ -1,6 +1,7 @@
 var question = document.querySelector("#question");
 var choices = array.from(document.querySelectorAll("#.choice-text"));
-var question = document.querySelector("#timer");
+var timer = document.querySelector("#timer");
+var scoreText = document.querySelector("#score");
 
 var cuurentQuestion = {};
 var acceptingAnswers = true;
@@ -10,35 +11,52 @@ var availableQuestions = [];
 
 var questionsArray = [
     {
-        question: "Who invented JavaScript?";
-        choiceA: "Brendan Eich";
-        choiceB: "Michael Jackson";
-        choiceC: "Brendan Schawb";
-        choiceD: "Lionel Messi";
+        question: "Who invented JavaScript?",
+        choiceA: "Brendan Eich",
+        choiceB: "Michael Jackson",
+        choiceC: "Brendan Schawb",
+        choiceD: "Lionel Messi",
         answer: 1;
     }
     {
-        question: "What is JavaScript";
-        choiceA: "A button on the keyboard";
-        choiceB: "Germany's national language";
-        choiceC: "A tool used to play a sport";
-        choiceD: "A programming language";
-        answer: 4;
+        question: "What is JavaScript",
+        choiceA: "A button on the keyboard",
+        choiceB: "Germany's national language",
+        choiceC: "A tool used to play a sport",
+        choiceD: "A programming language",
+        answer: 4
     }
     {
         question: "What is the HTML tag used to link your JavaScript code";
-        choiceA: "<js>;
-        choiceB: "<h1>";
-        choiceC: "<script>";
-        choiceD: "<meta>";
-        answer: 2;
+        choiceA: "<js>",
+        choiceB: "<h1>",
+        choiceC: "<script>",
+        choiceD: "<meta>",
+        answer: 2
     }
     {
         question: "Which of the following type of variable is visible everywhere in your JavaScript code?";
-        choiceA: "local variable";
-        choiceB: "global variable";
-        choiceC: "All of the above";
-        choiceD: "None of the above";
-        answer: 1;
+        choiceA: "local variable",
+        choiceB: "global variable",
+        choiceC: "All of the above",
+        choiceD: "None of the above",
+        answer: 2,
     }
 ]
+
+var scorePoints = 100;
+var amountOfQuestions = 4;
+
+startQuiz = function(){
+   score = 0;
+   questionCounter = 0;
+   availableQuestions = [...questionsArray];
+   getNewQuestion();
+}
+
+getNewQuestion = function(){
+    if(availableQuestions.lenght === 0 || questionCounter > amountOfQuestions){
+        localStorage.setItem('mostRecentScore', score)
+
+    }
+}
